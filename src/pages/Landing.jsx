984a7231d6454540245a612ae67dfd1397ec4b4b
Aybann <98073595+Aybann@users.noreply.github.com>
2023-10-19 17:11:38 -0700

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -14,20 +14,18 @@ const Landing = () => {
   }
 
   return ( 
-    <main  >
-      <div className='md:flex flex-row-reverse md:overflow-x-hidden'>
-        <div 
-           style={{
-            backgroundImage: `url(/src/assets/Hero\ Image.jpg))`,
-          }}
-          className='bg-image p-[12em] bg-center bg-cover md:p-[20em] mt-16 md:mt-0 md:basis-[40%]'></div>
+    <main className='overflow-y-hidden lg:h-screen' >
+      <div className='lg:flex flex-row-reverse'>
+        <div className='h-[20em] md:h-[30em] lg:h-fit overflow-hidden lg:overflow-visible mt-16 lg:mt-0 md:basis-[40%] '>
+          <img src={homeImage} alt="" className='w-full'/>
+        </div>
         <section className='px-4 md:basis-[60%]'>
           <div className='flex justify-between py-4 px-4 md:px-16 absolute top-0'>
             <a href="#">
               <img src={logo} alt="Yelp" />
             </a>
           </div>
-          <header className='my-6 font-semibold text-gray-500 md:flex justify-center items-center md:h-screen '>
+          <header className='my-6 font-semibold text-gray-500 lg:flex justify-center items-center lg:h-screen '>
             <div className='md:w-[60ch]'>
               <h1 className='text-4xl font-bold text-black md:text-6xl'>Explore the best<br /> camps on Earth.</h1>
               <p className=' my-4 md:w-[46ch]' >YelpCamp is a curated list of the best camping spots on Earth. Unfiltered and unbiased reviews</p>
